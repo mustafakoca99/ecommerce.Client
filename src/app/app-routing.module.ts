0c1755c5f6b5ecfe8662b324f57636385d28df79
Mustafa Koca <Mustafa.Koca@Vault.Insurance>
2024-01-07 18:50:24 +0300

    updated c and m

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './admin/layout/layout.component';
+import { DashboardComponent } from './admin/components/dashboard/dashboard.component';
+import { HomeComponent } from './ui/components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "admin", component: LayoutComponent, children: [
+      { path: "", component: DashboardComponent },
+      { path: "customer", loadChildren: () => import("./admin/components/customer/customer.module").then(x => x.CustomerModule) },
+      { path: "products", loadChildren: () => import("./admin/components/products/products.module").then(x => x.ProductsModule) },
+      { path: "orders", loadChildren: () => import("./admin/components/orders/orders.module").then(x => x.OrdersModule) },
+    ]
+  },
+  { path: "", component: HomeComponent },
+  { path: "baskets", loadChildren: () => import("./ui/components/baskets/baskets.module").then(x => x.BasketsModule) },
+  { path: "products", loadChildren: () => import("./ui/components/products/products.module").then(x => x.ProductsModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
